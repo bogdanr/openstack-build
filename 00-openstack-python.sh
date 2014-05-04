222b8f084c4994afcc210e25d26803839a43f1c6
Bogdan Radulescu <bogdan@nimblex.net>
2014-05-04 13:09:23 +0300

    openstack-python: From now on we'll put all the python stuff in a package.
    All dependencies and the OpenStack python files will be packaged together
    From here on we're going to focus on packing OpenStack for NimbleX

---
+++ 00-openstack-python.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+PKG="/tmp/openstack-python"
+
+easy_install pip
+
+build-requirements() {
+
+  cat {keystone,glance,nova,horizon}/requirements.txt | grep -vE "^#" | sort | uniq > requirements-all.txt
+
+  rm -rf $PKG
+  mkdir $PKG
+
+  pip install --root=$PKG pbr
+  pip install --root=$PKG MySQL-python	# This requires MySQL or MariaDB to be installed
+  pip install --root=$PKG -r requirements-all.txt
+  pip install --root=$PKG libvirt-python
+
+}
+
+build-openstack() {
+
+  for package in keystone glance nova horizon; do
+    cd $package
+    python setup.py install --root $PKG
+    cd ..
+  done
+
+}
+
+build-requirements
+build-openstack
+
+dir2lzm $PKG
+
